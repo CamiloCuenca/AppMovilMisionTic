91145d298c49fe1a815048b76a4e972ce49f86e7
CamiloCuenca <88161476+CamiloCuenca@users.noreply.github.com>
2021-12-02 14:47:39 -0500

    se creo la estructura de los usuarios

--- app/src/main/java/com/example/myappejemstic/HomeActivity.java
+++ app/src/main/java/com/example/myappejemstic/HomeActivity.java
@@ -12,19 +12,32 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.bumptech.glide.Glide;
 import com.example.myappejemstic.adapters.PaginasAdapter;
+import com.example.myappejemstic.pojos.Users;
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeActivity extends AppCompatActivity {
+
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference ref_user = database.getReference("users").child(user.getUid());
+
 
 
 
@@ -57,6 +70,12 @@ public class HomeActivity extends AppCompatActivity {
                         case 2:{
                             tab.setText("Solicitudes");
                             tab.setIcon(R.drawable.ic_solicitudes);
+                            BadgeDrawable badgeDrawable = tab.getOrCreateBadge();
+                            badgeDrawable.setBackgroundColor(
+                                    ContextCompat.getColor(getApplicationContext(),R.color.colorAccent)
+                            );
+                            badgeDrawable.setVisible(true);
+                            badgeDrawable.setNumber(1);
                             break;
                         }
                         case 3:{
@@ -78,13 +97,56 @@ public class HomeActivity extends AppCompatActivity {
 
         tabLayoutMediator.attach();
 
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                BadgeDrawable badgeDrawable=tabLayout.getTabAt(position).getOrCreateBadge();
+                badgeDrawable.setVisible(false);
+
+            }
+        });
+
 
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-
+        userunico();
 
 
     }//Fin del onecreate
+
+    private void userunico() {
+
+    ref_user.addListenerForSingleValueEvent(new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+            if(!snapshot.exists()){
+                Users uu = new Users(
+                        user.getUid(),
+                        user.getDisplayName(),
+                        user.getEmail(),
+                        user.getPhotoUrl().toString(),
+                        "desconectado",
+                        "2/12/2021",
+                        "2:41",
+                        0,
+                        0);
+
+                ref_user.setValue(uu);
+
+
+            }
+
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {
+
+        }
+    });
+
+    }
 
 
     @Override
